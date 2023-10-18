2d65099ba862c31606e3a9c9bad2c060ab152866
zhuyuhui <zhuyuhui@odianyun.com>
2023-10-18 15:45:03 +0800

    feat: 京东demo-补充一些工具类

--- example_normal/thirdlib_demos/demo_jingdong/lib/demo_jingdong/global/global_index.dart
+++ example_normal/thirdlib_demos/demo_jingdong/lib/demo_jingdong/global/global_index.dart
@@ -1,1 +1,2 @@
 export './providers/car_provider.dart';
+export './event_bus/event_bus_index.dart';
--- example_normal/thirdlib_demos/demo_jingdong/lib/demo_jingdong/tools/tools_index.dart
+++ example_normal/thirdlib_demos/demo_jingdong/lib/demo_jingdong/tools/tools_index.dart
@@ -4,3 +4,5 @@ export './MPSF/mpsf_index.dart';
 export './AMap/AmapManager.dart';
 export './factory_widgets.dart';
 export './Update/update.dart';
+export './Extension/extension_index.dart';
+export './Route/page_route_factory.dart';
